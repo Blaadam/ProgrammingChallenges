a52ba93038015519e544ba1b65b7cd1ee309b467
Blaadam <adam.worden06@gmail.com>
2024-07-17 14:54:20 +0100

    Factorial Solution

---
+++ ProjectSolutions/ProjectSolutions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxCount = 0;
+            maxCount = Int32.Parse(input);
+
+            int currentNumber = 1;
+
+            for (int i = 1; i <= maxCount; i++)
+            {
+                currentNumber = currentNumber * i;
+            }
+
+            Console.WriteLine(currentNumber);
+            Console.ReadKey();
+        }
+    }
+}

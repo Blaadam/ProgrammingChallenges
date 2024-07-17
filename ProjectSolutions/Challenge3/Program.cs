962e3c6ae6bd26aa44f916f1eee59c77dd65f2df
Blaadam <adam.worden06@gmail.com>
2024-07-17 21:26:12 +0100

    Challenge 3 - Permutations

---
+++ ProjectSolutions/Challenge3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3
+{
+    internal class Program
+    {
+        static List<string> GetPermutations(List<string> array, int start)
+        {
+            List<string> Permutations = new List<string>();
+
+            if (start == array.Count)
+            {
+                Console.WriteLine("[" + string.Join(", ", array) + "]");
+                return array;
+            }
+
+            for (int i = start; i < array.Count; i++)
+            {
+                (array[start], array[i]) = (array[i], array[start]);
+                List<string> Subpermutations = GetPermutations(array, start + 1);
+                (array[start], array[i]) = (array[i], array[start]);
+                Permutations.AddRange(Subpermutations);
+            }
+
+            return Permutations;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 4 consecutive numbers");
+            string AllNumbers = Console.ReadLine();
+            List<string> Numbers = new List<string>();
+            for (int i = 0; i < AllNumbers.Length; i++)
+            {
+                Numbers.Add(AllNumbers.Substring(i, 1));
+            }
+
+            Console.WriteLine(string.Join(", ", Numbers));
+
+            List<string> Permutations = GetPermutations(Numbers, 0);
+            Console.WriteLine(string.Join(", ", Permutations));
+
+            Console.ReadKey();
+        }
+    }
+}

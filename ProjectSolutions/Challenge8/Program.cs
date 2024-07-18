d170cdeb7e6a1744377a7fafdb69f2bbbcf1af24
Blaadam <adam.worden06@gmail.com>
2024-07-18 15:40:32 +0100

    Challenge 8 - Arithmetic Test

---
+++ ProjectSolutions/Challenge8/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string StudentName = Console.ReadLine();
+            if (string.IsNullOrEmpty(StudentName)) { return; }
+
+            List<bool> Answers = new List<bool> { };
+
+            for (int i = 0; i < 10; i++)
+            {
+                Question Quest = new Question();
+
+                Quest.GenateRandomNumbers();
+                Console.WriteLine($"{Quest.NumberA.ToString()} + {Quest.NumberB.ToString()} = ?");
+
+                bool IsCorrect = Quest.CheckAnswer(Console.ReadLine());
+                Answers.Add(IsCorrect);
+            }
+
+            int CorrectAnswers = Answers.Where(a => a == true).Count();
+
+            Console.WriteLine($"Your score: {CorrectAnswers.ToString()}/{Answers.Count().ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectSolutions/Challenge8/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge8
+{
+    public class Question
+    {
+        private static Random RNG = new Random(); // Make RNG static so it's shared across instances
+
+        public int NumberA { get; set; }
+        public int NumberB { get; set; }
+
+        public void GenateRandomNumbers()
+        {
+            NumberA = RNG.Next(0, 100);
+            NumberB = RNG.Next(0, 100);
+        }
+
+        public bool CheckAnswer(string InputAnswer)
+        {
+            int UserAnswer = 0;
+            Int32.TryParse(InputAnswer, out UserAnswer);
+
+            int CalculatedAnswer = NumberA + NumberB;
+            return CalculatedAnswer == UserAnswer;
+        }
+
+    }
+}

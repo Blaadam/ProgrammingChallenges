81c60e720dadfed7319d8934dfc00e0ccdacd6f1
Blaadam <adam.worden06@gmail.com>
2024-07-19 14:38:42 +0100

    Challenge 11 - Regex Query Tool

---
+++ ProjectSolutions/Challenge11/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Challenge11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string InputString = StringInputTb.Text;
+            string RegexString = RegexInputTb.Text;
+            
+            Regex Pattern = new Regex(RegexString);
+
+            string Output = Pattern.Match(InputString).ToString();
+            RegexOutputTb.Text = !string.IsNullOrEmpty(Output) ? Output : "Could not match the string to the provided Regex Pattern";
+        }
+    }
+}
